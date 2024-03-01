8c2f82d6569c6c29511a2b80629dfee8194daf74
striz <strizhov295@mail.ru>
2024-03-01 18:37:41 +0700

    review changes

--- scrapper/src/test/java/edu/java/scrapper/clients/StackOverflowClientTest.java
+++ scrapper/src/test/java/edu/java/scrapper/clients/StackOverflowClientTest.java
@@ -1,57 +1,69 @@
 package edu.java.scrapper.clients;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import edu.java.ScrapperApplication;
 import edu.java.clients.StackOverflowClient;
 import edu.java.dto.QuestionResponse;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = ScrapperApplication.class)
 @ActiveProfiles("test")
 public class StackOverflowClientTest {
-    @Rule
-    public WireMockRule wireMockRule = new WireMockRule();
+    private final static WireMockServer wireMockServer = new WireMockServer();
+
     @Autowired
     private StackOverflowClient stackOverflowClient;
+
+    @BeforeAll
+    public static void setUp() {
+        wireMockServer.start();
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        wireMockServer.stop();
+    }
 
     @Test
     public void get() {
         int id = 0;
         LocalDateTime dateTime = LocalDateTime.MIN;
-        configure(id, dateTime);
+        setUpServer(String.format("/questions/%d?site=stackoverflow", id), createBody(id, dateTime));
 
         QuestionResponse response = stackOverflowClient.get(String.valueOf(id));
         QuestionResponse.ItemResponse item = response.items().get(0);
 
-        assertThat(item.id()).isEqualTo(0);
+        assertThat(item.id()).isEqualTo(id);
         assertThat(item.lastActivity()).isEqualTo(dateTime.atOffset(ZoneOffset.UTC));
     }
 
-    private void configure(int id, LocalDateTime lastActvity) {
+    private String createBody(int id, LocalDateTime lastActvity) {
         String body = String.format(
             "{\"items\":[{\"last_activity_date\":%d,\"question_id\":%d}]}\n",
             lastActvity.toEpochSecond(ZoneOffset.UTC), id
         );
+        return body;
+    }
+
+    private void setUpServer(String url, String body) {
         configureFor("localhost", 8080);
-        stubFor(WireMock.get(urlEqualTo(String.format("/questions/%d?site=stackoverflow", id)))
-            .willReturn(aResponse()
-                .withStatus(HttpStatus.OK.value())
-                .withHeader("Content-Type", "application/json")
-                .withBody(body)));
+        wireMockServer.stubFor(
+            WireMock.get(urlEqualTo(url))
+                .willReturn(aResponse()
+                    .withStatus(200)
+                    .withHeader("Content-Type", "application/json")
+                    .withBody(body)));
     }
 }
