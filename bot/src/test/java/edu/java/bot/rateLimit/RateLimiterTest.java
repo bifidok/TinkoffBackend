336173df0c41f1c920e9418484c07180355be121
striz <strizhov295@mail.ru>
2024-03-29 11:30:03 +0700

    change bot and scrapper communication without rate limit

--- bot/src/test/java/edu/java/bot/rateLimit/RateLimiterTest.java
+++ bot/src/test/java/edu/java/bot/rateLimit/RateLimiterTest.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -34,11 +33,22 @@ public class RateLimiterTest {
 
         for (int i = 0; i < 10; i++) {
             mockMvc.perform(post("/updates")
+                    .with(request -> {
+                        request.setRemoteAddr("192.168.0.2");
+                        return request;
+                    })
                     .contentType(MediaType.APPLICATION_JSON)
                     .content(requestBody))
                 .andExpect(status().isOk());
         }
 
-        mockMvc.perform(post("/updates")).andExpect(status().isTooManyRequests());
+        mockMvc.perform(post("/updates")
+                .with(request -> {
+                    request.setRemoteAddr("192.168.0.2");
+                    return request;
+                })
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+            .andExpect(status().isTooManyRequests());
     }
 }
--- scrapper/src/test/java/edu/java/scrapper/rateLimit/RateLimiterTest.java
+++ scrapper/src/test/java/edu/java/scrapper/rateLimit/RateLimiterTest.java
@@ -10,8 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -19,7 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(classes = ScrapperApplication.class)
 @AutoConfigureMockMvc
-public class RateLimiterTest extends IntegrationTest{
+public class RateLimiterTest extends IntegrationTest {
     @Autowired
     private MockMvc mockMvc;
     @MockBean
@@ -30,9 +28,21 @@ public class RateLimiterTest extends IntegrationTest{
         int chatId = 1;
         when(linkService.findAll()).thenReturn(List.of(new Link()));
         for (int i = 0; i < 10; i++) {
-            mockMvc.perform(get("/links").header("tgChatId", chatId)).andExpect(status().isOk());
+            mockMvc.perform(get("/links")
+                    .with(request -> {
+                        request.setRemoteAddr("192.168.0.2");
+                        return request;
+                    })
+                    .header("tgChatId", chatId))
+                .andExpect(status().isOk());
         }
 
-        mockMvc.perform(get("/links").header("tgChatId", chatId)).andExpect(status().isTooManyRequests());
+        mockMvc.perform(get("/links")
+                .with(request -> {
+                    request.setRemoteAddr("192.168.0.2");
+                    return request;
+                })
+                .header("tgChatId", chatId))
+            .andExpect(status().isTooManyRequests());
     }
 }
